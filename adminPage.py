3210c64b2991e02481f5988f027d2b74c56bbbef
mhdalshaikh <57395008+mhdalshaikh@users.noreply.github.com>
2022-07-06 10:04:23 +0300

    Add files via upload

---
+++ adminPage.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+
+import mysql
+from PIL import Image
+import streamlit as st
+import pandas as pd
+import numpy as np
+import base64
+
+
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
+
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+set_bg_hack('C:\\Users\\mohammed_khalil\\Downloads\\background.png')
+
+image = Image.open("C:\\Users\\mohammed_khalil\\Downloads\\OIP.jpg")
+st.image(image)
+st.title('admin page name place holder')
+radio_selection = st.sidebar.selectbox('choose an option:', ('print reports', 'give permission','give privilege'))
+if radio_selection == 'print reports':
+    select_box_choice = st.selectbox('who to print for:', ('all', 'certain employee'))
+    if select_box_choice == 'all':
+        clm1, clm2 = st.columns(2)
+        date_range = clm1.date_input('from')
+        date_range2 = clm2.date_input('to')
+        clm1.button('download report')
+    elif select_box_choice == 'certain employee':
+        clm1, clm2, clm3,clm4 = st.columns(4)
+        ID = clm1.text_input('enter employee ID:')
+        clm2.text_input(label="", value="asdasd", disabled=True)
+        date_range = clm3.date_input('from')
+        date_range2 = clm4.date_input('to')
+        clm1.button('download report')
+elif radio_selection == 'give permission':
+    col1,col2=st.columns(2)
+    ID = col1.text_input('enter employee ID: ')
+    col2.text_input(label="",value="asdasd",disabled=True)
+    options = ('Customer site', 'Medical excuse', 'Vacation')
+    selection = st.selectbox("please choose a reason",
+                             options)
+
+    if selection == 'Customer site':
+        clm1, clm2, clm3, clm4, clm5 = st.columns(5)
+        client_name_1 = clm1.text_input('Client name: ')
+        loc1 = clm3.text_input('location:', key=1)
+        country3 = clm2.text_input('country:', key=3)
+        start_time1 = clm4.date_input('from:')
+        end_time1 = clm5.date_input('to:')
+        clm4.button('save/add')
+        save_exit_button = clm5.button('save/exit')
+    elif selection == 'Medical excuse':
+        clm1, clm2, clm3 = st.columns(3)
+        date_from = clm1.text_input('Hospital name: ')
+        start_time1 = clm2.date_input('from:')
+        end_time1 = clm3.date_input('to:')
+        clm2.button('save/add')
+        save_exit_button = clm3.button('save/exit')
+    elif selection == 'Vacation':
+        clm1, clm2, clm3 = st.columns(3)
+        start_time1 = clm2.date_input('from:')
+        end_time_2 = clm3.date_input('to:')
+        clm2.button('save/add')
+        save_exit_button = clm3.button('save/exit')
+elif radio_selection == 'give privilege':
+    col1, col2 = st.columns(2)
+    ID = col1.text_input('enter employee ID that you want to give privilege to : ')
+    col2.text_input(label="", value="asdasd", disabled=True)
+    col2.button('confirm')
---
+++ main.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import streamlit as st
+
+st.title('Late Sheet Name place holder')
+options = ('Customer visit', 'Hospital visit', 'Vendor visit', 'Business trip', 'Personal excuse', 'Reporting late')
+selection = st.selectbox("Dear NA u have been late for today's attendance for '00:00', please choose a reason", options)
+
+if selection == 'Customer visit':
+    clm1, clm2, clm3,clm4,clm5 = st.columns(5)
+    client_name_1 = clm1.text_input('Client name 1')
+    client_name_2 = clm1.text_input('Client name 2')
+    client_name_3 = clm1.text_input('Client name 3')
+    loc1=clm3.text_input('location:',key=1)
+    loc2=clm3.text_input('location:',key=2)
+    loc3=clm3.text_input('location:',key=3)
+    country1=clm2.text_input('country:',key=1)
+    country2=clm2.text_input('country:',key=2)
+    country3=clm2.text_input('country:',key=3)
+    start_time1 = clm4.time_input('from:', datetime.now(), 1)
+    end_time1 = clm5.time_input('to:', datetime.now(), 1)
+    start_time_2 = clm4.time_input('from:', datetime.now(), 2)
+    end_time_2 = clm5.time_input('to:', datetime.now(), 2)
+    start_time_3 = clm4.time_input('from:', datetime.now(), 3)
+    end_time_3 = clm5.time_input('to:', datetime.now(), 3)
+    clm4.button('save/add')
+    save_exit_button = clm5.button('save/exit')
+elif selection == 'Hospital visit':
+    clm1, clm2, clm3 = st.columns(3)
+    clm1.text_input('location')
+    start_time1 = clm2.time_input('from:', datetime.now(), 1)
+    end_time1 = clm3.time_input('to:', datetime.now(), 1)
+    clm2.button('save/add')
+    save_exit_button = clm3.button('save/exit')
+elif selection == 'Vendor visit':
+    clm1, clm2, clm3 = st.columns(3)
+    vendor_name_1 = clm1.text_input('vendor name 1')
+    vendor_name_2 = clm1.text_input('vendor name 2')
+    start_time1 = clm2.time_input('from:', datetime.now(), 1)
+    end_time1 = clm3.time_input('to:', datetime.now(), 1)
+    start_time_2 = clm2.time_input('from:', datetime.now(), 2)
+    end_time_2 = clm3.time_input('to:', datetime.now(), 2)
+    clm2.button('save/add')
+    save_exit_button = clm3.button('save/exit')
+elif selection == 'Business trip':
+    clm1, clm2, clm3,clm4 = st.columns(4)
+    clm1.text_input('country:')
+    clm2.text_input('location:')
+    date_from = clm3.date_input('from')
+    date_to = clm4.date_input('to')
+    clm3.button('save/add')
+    save_exit_button = clm4.button('save/exit')
+elif selection == 'Personal excuse':
+    clm1, clm2, clm3 = st.columns(3)
+    start_time1 = clm2.time_input('from:', datetime.now(), 1)
+    end_time1 = clm3.time_input('to:', datetime.now(), 1)
+    clm2.button('save/add')
+    save_exit_button = clm3.button('save/exit')
+elif selection == 'Reporting late':
+    clm1, clm2, clm3 = st.columns(3)
+    start_time1 = clm2.time_input('from:', datetime.now(), 1)
+    end_time1 = clm3.time_input('to:', datetime.now(), 1)
+    clm2.button('save/add')
+    save_exit_button = clm3.button('save/exit')
+
+
+
