c6bda6ae3785ec852ad6072789d3f8c73a69b958
mhdalshaikh <57395008+mhdalshaikh@users.noreply.github.com>
2022-08-08 11:07:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ import pyodbc
 import streamlit as st
 from PIL import Image
 
+# streamlit_app.py
+
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.ID}:")
+
+
 
 def init_connection():
     return pyodbc.connect(
